386b070b35dd7987067e74bf642c1e6615849a03
Adrian <adrian.bolinger@me.com>
2021-09-21 22:58:01 -0400

    Initial Commit

---
+++ MortgageCalcIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortgageCalcIssue
+//
+//  Created by Adrian Bolinger on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
